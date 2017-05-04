e92b836df5b451a6e069be5a21f36888f4bb7175
Артемий Козырев <tema@tema.ws>
2017-05-04 13:54:02 +0300

    init

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "extends": "react",
+  "plugins": [
+    "react"
+  ],
+  "rules": {
+    "indent": ["error", 2, { "SwitchCase": 1 }]
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './client/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.(png|jpg|svg)$/, loader: 'file-loader' },
+      { test: /\.css$/, loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader' }) },
+      { test: /\.scss$/, loader: ExtractTextPlugin.extract({ fallbackLoader: 'style-loader', loader: 'css-loader!postcss-loader!sass-loader'}) },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader'}
+    ]
+  },
+  plugins: [HtmlWebpackPluginConfig, new ExtractTextPlugin("bundle.css")]
+}
