d6bddcdcf6a9701cd17dae2e06ace1458d463862
TemaKozyrev <tema@tema.ws>
2017-05-10 21:37:05 +0300

    add text render

--- client/components/Text/index.js
+++ client/components/Text/index.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './styles.scss';
+import data from './text.json';
 
 export default class Text extends Component {
+  componentDidMount () {
+    console.log(data.text);
+  }
   render () {
     return (
       <div className="text">
-        text
+        { data.text.map((paragraph, i) => (<p key={i}>{paragraph}</p>))}
       </div>
     );
   }
