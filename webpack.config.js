77733fcd2fc8f3f8a6ec499aaa7b9080e21ad3f4
TemaKozyrev <tema@tema.ws>
2017-05-20 23:48:44 +0300

    fix webpack config codestyle

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,21 @@
+/*global require, module*/
+/*eslint no-undef: "error"*/
+
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
   template: './client/index.html',
   filename: 'index.html',
   inject: 'body'
-})
+});
 
 module.exports = {
   entry: './client/index.js',
   output: {
     path: path.resolve('dist'),
     filename: 'bundle.js',
-    publicPath: '/',
+    publicPath: '/'
   },
   module: {
     loaders: [
@@ -20,9 +23,12 @@ module.exports = {
       { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
       { test: /\.(png|jpg|svg)$/, loader: 'file-loader' },
       { test: /\.css$/, loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader' }) },
-      { test: /\.scss$/, loader: ExtractTextPlugin.extract({ fallbackLoader: 'style-loader', loader: 'css-loader!postcss-loader!sass-loader'}) },
-      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader'}
+      { test: /\.scss$/,
+        loader: ExtractTextPlugin.extract({
+          fallbackLoader: 'style-loader',
+          loader: 'css-loader!postcss-loader!sass-loader' }) },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader' }
     ]
   },
   plugins: [HtmlWebpackPluginConfig, new ExtractTextPlugin("bundle.css")]
-}
+};
